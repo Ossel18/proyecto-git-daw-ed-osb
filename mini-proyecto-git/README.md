82f1f8e006bc46d632f440c13ca0ef77d53ae666
Ossel18 <osantamariab01@educantabria.es>
2025-12-16 16:12:24 +0100

    Creación de main.java

--- mini-proyecto-git/README.md
+++ mini-proyecto-git/README.md
@@ -12,4 +12,5 @@ Práctica realizada por: OSB
 Pasos para hacer el mini proyecto
 1. Creación del README.
 2. Creación de la carpeta src.
+3. Creación de main.java.
 >>>>>>> 4bf249fc702c2b413ddeb36cb2abc5c3b8a67f78:READMEOSB.md
---
+++ mini-proyecto-git/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holaclase;
+
+/**
+ *
+ * @author DAW120
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bienvenido, " + args[0]);
+    }
+}
